984598938f392d31dc6ef43ccb81378c396438e4
Ubuntu <azureuser@NMTGPU.ro5yjgl5uiterouvoskps5fytb.bx.internal.cloudapp.net>
2021-07-16 08:25:36 +0000

    added weighted_attn

--- onmt/decoders/__init__.py
+++ onmt/decoders/__init__.py
@@ -3,11 +3,14 @@ from onmt.decoders.decoder import DecoderBase, InputFeedRNNDecoder, \
     StdRNNDecoder
 from onmt.decoders.transformer import TransformerDecoder, TransformerLMDecoder
 from onmt.decoders.cnn_decoder import CNNDecoder
+from onmt.decoders.weighted_transformer import WeightedTransformerDecoder
 
 
 str2dec = {"rnn": StdRNNDecoder, "ifrnn": InputFeedRNNDecoder,
            "cnn": CNNDecoder, "transformer": TransformerDecoder,
-           "transformer_lm": TransformerLMDecoder}
+           "transformer_lm": TransformerLMDecoder,
+           "weighted_transformer":WeightedTransformerDecoder}
 
 __all__ = ["DecoderBase", "TransformerDecoder", "StdRNNDecoder", "CNNDecoder",
-           "InputFeedRNNDecoder", "str2dec", "TransformerLMDecoder"]
+           "InputFeedRNNDecoder", "str2dec", "TransformerLMDecoder",
+           "WeightedTransformerDecoder"]
